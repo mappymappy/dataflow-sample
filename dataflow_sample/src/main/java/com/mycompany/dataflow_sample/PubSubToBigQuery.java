7a276bcd9a1c11bef4c38af36612afece791068b
marnie <mappymappy0301@i.softbank.jp>
2018-03-20 17:27:27 +0900

    refactoring

--- dataflow_sample/src/main/java/com/mycompany/dataflow_sample/PubSubToBigQuery.java
+++ dataflow_sample/src/main/java/com/mycompany/dataflow_sample/PubSubToBigQuery.java
@@ -6,7 +6,6 @@
 package com.mycompany.dataflow_sample;
 
 
-import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
 import com.mycompany.dataflow_sample.converter.BigQueryRowConverter;
 import com.mycompany.dataflow_sample.partition.DayPartitionDestinations;
@@ -18,8 +17,11 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.values.PCollection;
+import org.joda.time.Duration;
+
 
 
 public class PubSubToBigQuery {
@@ -29,13 +31,13 @@ public class PubSubToBigQuery {
      */
     public static void main(String[] args) {
         ResourceBundle config = ResourceBundle.getBundle("config");
-        PubSubToBigQuery(config);        
+        PubSubToBigQuery(config);
     }
-        
+
     private static void PubSubToBigQuery(ResourceBundle config) {
         // create option
         DataflowPipelineOptions options = PipelineOptionsFactory.create()
-            .as(DataflowPipelineOptions.class);
+                .as(DataflowPipelineOptions.class);
         options.setProject(config.getString("targetProject"));
         options.setStagingLocation(config.getString("targetStagingLocation"));
         options.setTempLocation(config.getString("targetTempLocation"));
@@ -44,23 +46,15 @@ public class PubSubToBigQuery {
         options.setJobName(config.getString("targetJobName"));
         // create pipeline & get inputData
         Pipeline p = Pipeline.create(options);
-        PCollection<String> streamData;
-        streamData = p.apply(PubsubIO.readStrings()
-                .fromSubscription(config.getString("targetSubscription"))
-        );
-        // apply convertRule (from:string,to:TableRow) 
-        PCollection<TableRow> rows = streamData.apply(
-            ParDo
-            .of(new BigQueryRowConverter())
-        );
-        // output
         TableSchema schema = SampleSchemaFactory.create();
-        rows.apply("WriteToBQ", BigQueryIO.writeTableRows()
-            .to(new DayPartitionDestinations(config.getString("targetBigQueryTable")))
-            .withSchema(schema)
-            .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
-            .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
-
+        p.apply(PubsubIO.readStrings().fromSubscription(config.getString("targetSubscription")))
+                .apply(Window.<String>into(FixedWindows.of(Duration.standardMinutes(5))))
+                .apply(ParDo.of(new BigQueryRowConverter()))
+                .apply("WriteToBQ", BigQueryIO.writeTableRows()
+                        .to(new DayPartitionDestinations(config.getString("targetBigQueryTable")))
+                        .withSchema(schema)
+                        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+                        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
         p.run();
-    }     
+    }
 }
